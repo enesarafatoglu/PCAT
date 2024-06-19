ba4ddc90a939ae2e86e011d6bd33af5e1c89072d
Enes Arafatoglu <enessarafatogluu@gmail.com>
2024-06-20 00:58:53 +0300

    mvc düzenine geçildi

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override')
 const ejs = require('ejs');
-const path = require('path');
-const Photo = require('./models/Photo');
+const photoController = require('./controllers/photoControllers');
+const pageController = require('./controllers/pageControllers');
 
 const app = express();
 
@@ -22,34 +24,22 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
+app.use(methodOverride('_method', {
+  methods: ['POST', 'GET'],
+}));
 
 //ROUTES
-app.get('/', async (req, res) => {
-  const photos = await Photo.find({})
-  res.render('index', {
-    photos
-  });
-});
+app.get('/', photoController.getAllPhotos);
+app.get('/photos/:id', photoController.getPhoto);
+app.post('/photos', photoController.createPhoto);
+app.put('/photos/:id', photoController.updatePhoto);
+app.delete('/photos/:id', photoController.deletePhoto);
 
-app.get('/photos/:id', async (req, res) => {
-  const photo = await Photo.findById(req.params.id);
-  res.render('photo', {
-    photo
-  });
-});
+app.get('/about', pageController.getAboutPage);
+app.get('/add', pageController.getAddPage);
+app.get('/photos/edit/:id', pageController.getEditPage);
 
-app.get('/about', (req, res) => {
-  res.render('about');
-});
-
-app.get('/add', (req, res) => {
-  res.render('add');
-});
-
-app.post('/photos', async (req, res) => {
-  await Photo.create(req.body)
-  res.redirect('/');
-});
 
 const port = 3000;
 
