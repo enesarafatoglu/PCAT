b7d8f7a31c839c90be354723a32c2502ce02a663
Enes Arafatoglu <enessarafatogluu@gmail.com>
2024-06-19 16:43:20 +0300

    mongoose dahil edildi

--- app.js
+++ app.js
@@ -1,24 +1,44 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
 const path = require('path');
+const Photo = require('./models/Photo');
 
 const app = express();
+
+mongoose
+  .connect('mongodb://localhost/pcat-test-db')
+  .then(() => {
+    console.log('Database connection successful');
+  })
+  .catch((err) => {
+    console.error('Database connection error:', err);
+  });
 
 //TEMPLATE ENGINE
 app.set('view engine', 'ejs');
 
 //MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //ROUTES
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({})
+  res.render('index', {
+    photos
+  });
 });
 app.get('/about', (req, res) => {
   res.render('about');
 });
 app.get('/add', (req, res) => {
   res.render('add');
+});
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body)
+  res.redirect('/');
 });
 
 const port = 3000;
